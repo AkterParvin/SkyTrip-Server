baa5f70ca72ab65570cfa659d845cde6660eb775
connect-Parvin <parvin.akter0190@gmail.com>
2023-11-30 14:50:15 +0600

    some update for guide route api

--- index.js
+++ index.js
@@ -38,6 +38,46 @@ async function run() {
         const wishlistCollection = client.db('skyTripDB').collection('wishlist');
         const bookingCollection = client.db('skyTripDB').collection('bookings');
         const storiesCollection = client.db('skyTripDB').collection('stories');
+        const guidesCollection = client.db('skyTripDB').collection('guides');
+
+
+        // jwt related api 
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "6h" });
+            res.send({ token });
+        })
+
+        //verify access token in middleware
+        const verifyToken = (req, res, next) => {
+            console.log("access-token from verify token");
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: "Unauthorized Access" });
+            }
+            const token = req.headers.authorization.split(" ")[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({
+                        message: "Unauthorized Access"
+                    });
+                }
+                req.decoded = decoded;
+                next();
+            })
+            // next();
+        };
+        // verifyAdmin middleware
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const isAdmin = user?.role === "admin";
+            if (!isAdmin) {
+                return res.status(403).send({ message: "forbidden access" }); 
+            };
+            next();
+        }
+
 
 
 
@@ -55,28 +95,55 @@ async function run() {
 
         // users related api 
         app.get("/users", async (req, res) => {
+            // console.log(req.headers);
             const result = await usersCollection.find().toArray();
             res.send(result);
         });
 
-        app.get("/users/admin/:email", async (req, res) => {
+        // find admin or guide with id query 
+        app.get("/users/role/:email",verifyToken,async (req, res) => {
             const email = req.params.email;
             // if (email !== req.decoded.email) {
             //     return res.status(403).send({ message: "forbidden access" });
             // }
-            const query = { email: email };
-            // after token use above commented codes 
+            const query = { email: email }; 
             const user = await usersCollection.findOne(query);
-            let admin = false;
-            if (user) {
-                admin = user?.role === "admin";
+
+            if (!user) {
+                return res.status(404).send({ message: "User not found" });
+
             }
-            res.send({ admin });
+            const role = user.role
+            res.send({ role });
 
         })
 
+        // find admin 
+        // app.get("/users/admin/:email", verifyToken, async (req, res) => {
+        //     const email = req.params.email;
+        //     if (email !== req.decoded.email) {
+        //         return res.status(403).send({ message: "forbidden access" });
+        //     }
+        //     const query = { email: email };
+        //     const user = await usersCollection.findOne(query);
+        //     let admin = false;
+        //     if (user) {
+        //         admin = user?.role === "admin";
+        //     }
+        //     res.send({ admin });
+        // });
+
+     
 
 
+        //find all guides name ,id and emails 
+        app.get("/users/:role", async (req, res) => {
+            const role = req.params.role.toLowerCase();
+            const result = await usersCollection.find({ role }).toArray();
+            res.send(result);
+        })
+
+        // post a new user 
         app.post("/users", async (req, res) => {
             const user = req.body;
             const query = { email: user.email };
@@ -88,28 +155,32 @@ async function run() {
             res.send(result);
         });
 
-        app.patch("/users/admin/:id", async (req, res) => {
+        app.patch("/users/role/:id", async (req, res) => {
             const id = req.params.id;
-            const filter = { _id: new ObjectId(id) };
+            const role = req.body.role;
+            // console.log(role);
+            if (!["admin", "guide"].includes(role)) {
+                return res.status(400).send({ message: "Invalid Role" });
+            }
+            const query = { _id: new ObjectId(id) };
             const updatedDoc = {
-                $set: {
-                    role: "admin"
-                }
-            };
-            const result = await usersCollection.updateOne(filter, updatedDoc);
-            res.send(result);
+                $set: { role }
+            }
+            const result = await usersCollection.updateOne(query, updatedDoc);
+            res.send(result)
+
         })
-        app.patch("/users/guide/:id", async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) };
-            const updateDoc = {
-                $set: {
-                    role: "guide"
-                }
-            };
-            const result = await usersCollection.updateOne(filter, updateDoc);
-            res.send(result);
-        })
+        // app.patch("/users/guide/:id", async (req, res) => {
+        //     const id = req.params.id;
+        //     const filter = { _id: new ObjectId(id) };
+        //     const updateDoc = {
+        //         $set: {
+        //             role: "guide"
+        //         }
+        //     };
+        //     const result = await usersCollection.updateOne(filter, updateDoc);
+        //     res.send(result);
+        // })
         app.delete("/users/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
@@ -150,27 +221,38 @@ async function run() {
         //     const result = await bookingCollection.find().toArray();
         //     res.send(result);
         // });
+        
         app.get('/bookings', async (req, res) => {
-            const email = req.query.email;
-            const query = { email: email };
+            let query = {};
+            if (req.query.tourist_email) {
+                query = { tourist_email: req.query.tourist_email }
+            }
             const result = await bookingCollection.find(query).toArray();
             res.send(result);
 
         })
- 
+
+
         app.get("/bookings", async (req, res) => {
             const result = await bookingCollection.find().toArray();
-            res.send(result); 
+            res.send(result);
+        });
+        app.get("/bookings/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bookingCollection.findOne(query);
+            res.send(result);
         })
-        // app.get('/pendingservices', async (req, res) => {
-        //     console.log("Token owner info:", req.user);
-        //     let query = {};
-        //     if (req.query.provider_email) {
-        //         query = { provider_email: req.query.provider_email }
-        //     }
-        //     const result = await bookingCollection.find(query).toArray();
-        //     res.send(result);
-        // })
+
+        app.get('/assignedtours', async (req, res) => {
+            console.log("Token owner info:", req.user);
+            let query = {};
+            if (req.query.guide_email) {
+                query = { guide_email: req.query.guide_email }
+            }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        })
 
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
@@ -186,24 +268,24 @@ async function run() {
             res.send(result);
         });
 
-        // app.patch('/bookings/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const filter = { _id: new ObjectId(id) }
-        //     const updateBooking = req.body;
-        //     const updateDoc = {
-        //         $set: {
-        //             status: updateBooking.status
-        //         },
-        //     }
-        //     console.log(updateBooking);
-        //     const result = await bookingCollection.updateOne(filter, updateDoc);
-        //     res.send(result);
-        // })
+        app.patch('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateBooking = req.body;
+            const updatedDoc = {
+                $set: {
+                    status: updateBooking.status
+                },
+            }
+            console.log(updateBooking);
+            const result = await bookingCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
 
         // story related api 
         app.get("/stories", async (req, res) => {
             const result = await storiesCollection.find().toArray();
-            res.send(result); 
+            res.send(result);
         })
         app.get("/stories", async (req, res) => {
             let query = {};
@@ -212,7 +294,7 @@ async function run() {
             }
             const result = await bookingCollection.find(query).toArray();
             res.send(result);
- 
+
         })
         app.post("/stories", async (req, res) => {
             const stories = req.body;
@@ -226,7 +308,26 @@ async function run() {
             res.send(result);
         });
 
-
+        // guide related api 
+        app.get("/guides", async (req, res) => {
+            const result = await guidesCollection.find().toArray();
+            res.send(result);
+        });
+        app.get("/guides", async (req, res) => {
+            // let query = {};
+            // if (req.query.guide_email) {
+            //     query = { guide_email: req.query.guide_email }
+            // }
+            const guide_email = req.query.guide_email;
+            const query = { guide_email: guide_email };
+            const result = await guidesCollection.findOne(query);
+            res.send(result);
+        });
+        app.get("/guides/:guide_email", async (req, res) => {
+            const query = { guide_email: req.params.guide_email };
+            const result = await guidesCollection.findOne(query);
+            res.send(result);
+        })
 
 
         // await client.connect();
